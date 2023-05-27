c88984d1f671e5c1d6f753277259317780ad14ea
gokulmaxi <gokulakams@gmail.com>
2023-05-27 12:41:36 +0530

    [UPDATE]added animation toggle for icons and other dom manipulations

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,6 +1,11 @@
 const choices = document.querySelectorAll(".options button");
 const result = document.getElementById("result");
 const play = document.getElementById("play");
+const restart = document.getElementById("restart");
+const systemIcon = document.getElementById("systemIcon")
+const playerIcon = document.getElementById("playerIcon")
+const playerScoreElement = document.getElementById("playerScore")
+const computerScoreElement = document.getElementById("systemScore")
 
 let playerScore = 0;
 let computerScore = 0;
@@ -11,14 +16,27 @@ choices.forEach((choice) => {
 });
 play.addEventListener("click", function () {
   choices.forEach((choice) => {
+    playerIcon.style.animationPlayState = "running";
+    systemIcon.style.animationPlayState = "running";
     choice.disabled = false;
     choice.classList.remove("disabled")
   });
 });
+restart.addEventListener("click", function () { location.reload() })
 
 function playGame(e) {
   const playerChoice = e.target.id;
   const computerChoice = getComputerChoice();
+  playerIcon.src = `assets/img/${playerChoice}.png`;
+  systemIcon.src = `assets/img/${computerChoice}.png`;
+  playerIcon.style.animationPlayState = "paused";
+  systemIcon.style.animationPlayState = "paused";
+  setTimeout(function () {
+    playerIcon.src = `assets/img/rock.png`;
+    systemIcon.src = `assets/img/rock.png`;
+    playerIcon.style.animationPlayState = "running";
+    systemIcon.style.animationPlayState = "running";
+  }, 1000);
   const winner = getWinner(playerChoice, computerChoice);
 
   showResult(playerChoice, computerChoice, winner);
@@ -47,11 +65,14 @@ function getWinner(player, computer) {
 }
 
 function showResult(playerChoice, computerChoice, winner) {
-  result.innerHTML = `
-    <p>You choose ${playerChoice}</p>
-    <p>Computer choose ${computerChoice}</p>
-    <p>${winner}</p>
-    <p>Player: ${playerScore}</p>
-    <p>Computer: ${computerScore}</p>
-  `;
+  result.textContent = winner
+  playerScoreElement.textContent = playerScore;
+  computerScoreElement.textContent = computerScore;
+  // result.innerHTML = `
+  //   <p>You choose ${playerChoice}</p>
+  //   <p>Computer choose ${computerChoice}</p>
+  //   <p>${winner}</p>
+  //   <p>Player: ${playerScore}</p>
+  //   <p>Computer: ${computerScore}</p>
+  // `;
 }
